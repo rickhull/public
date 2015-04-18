e53895df9e71023d630d1b59609451f59dba1f75
Rick Hull <rick.hull@gmail.com>
2015-04-17 21:03:17 -0700

    tighten

--- etc/profile.d/ssh-agent.sh
+++ etc/profile.d/ssh-agent.sh
@@ -1,22 +1,17 @@
-if [ -d ~/.ssh ] && [ "$EUID" -gt 999 ]; then
-
+if [ -d ~/.ssh ] && [ $EUID -gt 999 ]; then
     # we only care about our own agents which were started by init (PID 1)
     PGREP_FLAGS="-u $EUID --parent 1"
-
     run_agent () {
 	pkill ssh-agent $PGREP_FLAGS
 	ssh-agent > ~/.ssh/agent
 	chmod 600 ~/.ssh/agent
     }
 
-
     # do I already have a single ssh-agent running?
     # if so, does its PID match ~/.ssh/agent?
     # if so, source ~/.ssh/agent
     # otherwise, kill any existing agent and write to ~/.ssh/agent
-
     AGENT_PID=`pgrep ssh-agent $PGREP_FLAGS`
-
     if [ -z "$AGENT_PID" ] || \
 	   [ `echo "$AGENT_PID" | wc -l` -ne 1 ] || \
 	   [ ! -e ~/.ssh/agent ] || \
