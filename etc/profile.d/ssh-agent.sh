e37c0545da88f16fadfc2af94989428ae4d002c3
Rick Hull <rick.hull@gmail.com>
2015-04-17 19:24:06 -0700

    don't use bashisms
    - function is a bash builtin
    - use sh syntax, because gdm (e.g.) sources profile with sh
    - bashisms break gdm

--- etc/profile.d/ssh-agent.sh
+++ etc/profile.d/ssh-agent.sh
@@ -1,27 +1,30 @@
-if [ -d ~/.ssh ]; then
+if [ -d ~/.ssh ] && [ "$EUID" -gt 999 ]; then
 
-    function run_agent {
-	pkill ssh-agent -u $EUID
+    # we only care about our own agents which were started by init (PID 1)
+    PGREP_FLAGS="-u $EUID --parent 1"
+
+    run_agent () {
+	pkill ssh-agent $PGREP_FLAGS
 	ssh-agent > ~/.ssh/agent
 	chmod 600 ~/.ssh/agent
-	echo "ssh-add your keys"
     }
 
 
-    # do I already have an ssh-agent running?
+    # do I already have a single ssh-agent running?
     # if so, does its PID match ~/.ssh/agent?
     # if so, source ~/.ssh/agent
     # otherwise, kill any existing agent and write to ~/.ssh/agent
 
-    AGENT_PID=`pgrep ssh-agent -u $EUID`
+    AGENT_PID=`pgrep ssh-agent $PGREP_FLAGS`
 
-    if [ -z "$AGENT_PID" ]; then
-	run_agent
-    elif [ ! -e ~/.ssh/agent ]; then
-	run_agent
-    elif ! grep "$AGENT_PID" ~/.ssh/agent > /dev/null; then
+    if [ -z "$AGENT_PID" ] || \
+	   [ `echo "$AGENT_PID" | wc -l` -ne 1 ] || \
+	   [ ! -e ~/.ssh/agent ] || \
+	   ! grep "$AGENT_PID" ~/.ssh/agent > /dev/null
+    then
 	run_agent
     fi
 
     source ~/.ssh/agent
+    ssh-add -l # list keys
 fi
