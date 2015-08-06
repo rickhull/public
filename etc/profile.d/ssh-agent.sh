2276582af915d6726e19b32b12c1cbff25698919
Rick Hull <rick.hull@gmail.com>
2015-08-06 00:45:51 -0700

    this works under Debian Jessie Gnome and SSH and su

--- etc/profile.d/ssh-agent.sh
+++ etc/profile.d/ssh-agent.sh
@@ -1,25 +1,45 @@
-if [ -d ~/.ssh ] && [ $EUID -gt 999 ]; then
-    # we only care about our own agents which were started by init (PID 1)
-    PGREP_FLAGS="-u $EUID --parent 1"
+# WARNING: this file is sourced using `sh` and not just `bash`
+#          DO NOT USE bashisms unless you want to have a mystery on your hands
+
+SSH_DIR="$HOME/.ssh"
+USERID=$(id -u)  # UID and EUID are empty when loaded by graphical shell
+
+if [ -d "$SSH_DIR" ] && [ "$USERID" -gt 999 ]; then
+    AGENT_FILE="$SSH_DIR/agent"       # stores the output of ssh-agent
+    PGREP_FLAGS="-u $USERID --parent 1" # only our agent(s), started by init
+    ERROR=""
+
+    agent_pid() {
+	pgrep ssh-agent $PGREP_FLAGS
+    }
     run_agent () {
 	pkill ssh-agent $PGREP_FLAGS
-	ssh-agent > ~/.ssh/agent
-	chmod 600 ~/.ssh/agent
+	ssh-agent > $AGENT_FILE
+	chmod 600 $AGENT_FILE
+	ERROR=""
     }
 
-    # do I already have a single ssh-agent running?
-    # if so, does its PID match ~/.ssh/agent?
-    # if so, source ~/.ssh/agent
-    # otherwise, kill any existing agent and write to ~/.ssh/agent
-    AGENT_PID=`pgrep ssh-agent $PGREP_FLAGS`
-    if [ -z "$AGENT_PID" ] || \
-	   [ `echo "$AGENT_PID" | wc -l` -ne 1 ] || \
-	   [ ! -e ~/.ssh/agent ] || \
-	   ! grep "$AGENT_PID" ~/.ssh/agent > /dev/null
-    then
+    if [ ! -e $AGENT_FILE ]; then ERROR="no agent file"; fi
+    if [ $(agent_pid | wc -l) -ne 1 ]; then ERROR="num_agents != 1"; fi
+    if [ -n "$ERROR" ]; then run_agent; fi
+
+    # at this point, we have an agent file, and one agent running
+    # source the agent file
+    . $AGENT_FILE
+
+    # validate PID and SOCK
+    if [ "$SSH_AGENT_PID" != $(agent_pid) ]; then ERROR="PID mismatch"; fi
+    if [ ! -e "$SSH_AUTH_SOCK" ]; then ERROR="SSH_AUTH_SOCK file missing"; fi
+    if [ -n "$ERROR" ]; then
 	run_agent
+	. $AGENT_FILE
     fi
 
-    source ~/.ssh/agent
+    unset AGENT_FILE
+    unset PGREP_FLAGS
+    unset ERROR
     ssh-add -l # list keys
 fi
+
+unset SSH_DIR
+unset USERID
